dc148aa70d9678d0f3445f4be98be3114a9adadf
czarbitoon <czarbitoon@gmail.com>
2025-01-27 10:41:31 +0800

    Auth Fix

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -12,30 +12,62 @@ const AuthProvider = ({ children }) => {
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (token) {
-      setIsAuthenticated(true);
-      axios.get('http://127.0.0.1:8000/api/profile', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-        .then((response) => {
-          setUser (response.data);
+      const expirationTime = localStorage.getItem('expirationTime');
+      const currentTime = new Date().getTime();
+      if (currentTime < expirationTime) {
+        setIsAuthenticated(true);
+        axios.get('http://127.0.0.1:8000/api/profile', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         })
-        .catch((error) => {
-          console.error('Error fetching profile:', error);
-        });
+          .then((response) => {
+            setUser (response.data);
+          })
+          .catch((error) => {
+            console.error('Error fetching profile:', error);
+          });
+      } else {
+        // Token is expired, refresh it
+        axios.post('http://127.0.0.1:8000/api/token/refresh', {
+          refresh_token: localStorage.getItem('refreshToken'),
+        })
+          .then((response) => {
+            localStorage.setItem('token', response.data.access_token);
+            localStorage.setItem('expirationTime', response.data.expires_in * 1000 + currentTime);
+            setIsAuthenticated(true);
+            axios.get('http://127.0.0.1:8000/api/profile', {
+              headers: {
+                Authorization: `Bearer ${response.data.access_token}`,
+              },
+            })
+              .then((response) => {
+                setUser (response.data);
+              })
+              .catch((error) => {
+                console.error('Error fetching profile:', error);
+              });
+          })
+          .catch((error) => {
+            console.error('Error refreshing token:', error);
+          });
+      }
     } else {
       setIsAuthenticated(false);
     }
   }, []);
 
-  const login = (token) => {
+  const login = (token, refreshToken, expirationTime) => {
     localStorage.setItem('token', token);
+    localStorage.setItem('refreshToken', refreshToken);
+    localStorage.setItem('expirationTime', expirationTime);
     setIsAuthenticated(true);
   };
 
   const logout = () => {
     localStorage.removeItem('token');
+    localStorage.removeItem('refreshToken');
+    localStorage.removeItem('expirationTime');
     setIsAuthenticated(false);
   };
 
